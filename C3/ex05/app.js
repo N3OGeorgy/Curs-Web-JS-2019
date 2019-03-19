c09010be8810a2b782ae16a618a793083ef99f2f
N3OGeorgy <30291201+N3OGeorgy@users.noreply.github.com>
2019-03-19 20:47:47 +0200

    Curs 3 Up

--- C3/ex05/app.js
+++ C3/ex05/app.js
@@ -1,1 +1,45 @@
 console.log('ceva');
+var person = {
+  name: 'Dr',
+  surname: 'I',
+  age: 32,
+  petOwner: false,
+  skills: ['html', 'js', 'css'],
+  friends: [{
+    name: 'L',
+    surname: 'L',
+    age: 30
+  },
+{
+  name: 'S',
+  surname: 'S',
+  age: 30
+},
+{
+  name: 'C',
+  surname: 'C',
+  age: 32
+}]
+};
+
+console.log(person.friends);
+console.log('My name is: ' + person.name + ' si stiu ' + person.skills['0'] + ' si ' + person.skills['2']);
+
+var string;
+var count = person.friends.length;
+
+var i = 0;
+person.friends.forEach(function(element){
+  i++;
+  var end;
+  if(count-1 == i)
+    end = ' si ';
+    else
+    end = ', ';
+  if(count==i)
+  string += element.name;
+  else
+  string += element.name + end;
+});
+
+console.log('Am ' + person.friends.length + ' prieteni: ' + string);

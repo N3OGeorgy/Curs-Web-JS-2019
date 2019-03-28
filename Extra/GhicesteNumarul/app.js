697c25c12d291ff354edd3e02e11b672a50b7243
N3OGeorgy <N3OGeorgy@gmail.com>
2019-03-28 21:45:43 +0200

    Extra: Numarul Up3

--- Extra/GhicesteNumarul/app.js
+++ Extra/GhicesteNumarul/app.js
@@ -1,5 +1,6 @@
 const theNumber = Math.floor(Math.random() * 101);
 let gameWon = false;
+console.log(theNumber);
 var my_func = function(event) {
     event.preventDefault();
     let numberInput = parseInt(document.getElementById("Input1").value);
@@ -11,14 +12,17 @@ var my_func = function(event) {
       switch (true) {
         case rezultatul > 0:
           document.getElementById("result").innerHTML = numberInput + ' este prea mic.';
+          document.getElementById('Input1').value = '';
         break;
         case rezultatul < 0:
           document.getElementById("result").innerHTML = numberInput + ' este prea mare.';
+          document.getElementById('Input1').value = '';
         break;
         case rezultatul == 0:
           // document.getElementById("result").className = "alert alert-success";
           document.getElementById("result").style.display = "none";
           document.getElementById("image").style.display = "block";
+          document.getElementById("image").classList.add('image');
           document.getElementById("result").innerHTML = numberInput + ' este corect!';
           gameWon = true;
         break;
@@ -31,3 +35,4 @@ var my_func = function(event) {
 };
 var form = document.getElementById("numarul");
 form.addEventListener("submit", my_func, true);
+document.getElementById("Input1").focus();

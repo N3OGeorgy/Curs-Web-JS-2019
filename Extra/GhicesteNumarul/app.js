98681d8bd24d4a8cf9c857bdcf43ff0bb2847400
N3OGeorgy <N3OGeorgy@gmail.com>
2019-03-28 21:34:02 +0200

    Extra: Numarul Update

--- Extra/GhicesteNumarul/app.js
+++ Extra/GhicesteNumarul/app.js
@@ -1,10 +1,14 @@
 const theNumber = Math.floor(Math.random() * 101);
+let gameWon = false;
+console.log(theNumber);
 var my_func = function(event) {
     event.preventDefault();
     let numberInput = parseInt(document.getElementById("Input1").value);
     if(numberInput){
       let rezultatul = theNumber - numberInput;
-      document.getElementById("result").style.display = "block";
+      if(gameWon == false){
+        document.getElementById("result").style.display = "block";
+      }
       switch (true) {
         case rezultatul > 0:
           document.getElementById("result").innerHTML = numberInput + ' este prea mic.';
@@ -13,8 +17,11 @@ var my_func = function(event) {
           document.getElementById("result").innerHTML = numberInput + ' este prea mare.';
         break;
         case rezultatul == 0:
-          document.getElementById("result").className = "alert alert-success";
+          // document.getElementById("result").className = "alert alert-success";
+          document.getElementById("result").style.display = "none";
+          document.getElementById("image").style.display = "block";
           document.getElementById("result").innerHTML = numberInput + ' este corect!';
+          gameWon = true;
         break;
         default:
       }

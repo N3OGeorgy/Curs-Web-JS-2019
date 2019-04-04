16e554c4fd90f98eb3e1079aeca01cc95d4951a2
N3OGeorgy <HomeDesktop+N3OGeorgy@users.noreply.github.com>
2019-04-04 18:04:32 +0300

    Extra: Ghiceste Numarul Update

--- Extra/GhicesteNumarul/app.js
+++ Extra/GhicesteNumarul/app.js
@@ -1,32 +1,54 @@
 const theNumber = Math.floor(Math.random() * 101);
 let gameWon = false;
-console.log(theNumber);
+let gameContinues = true;
+let nrIncercari = 0;
+const maxIncercari = 10;
+
+function incercari() {
+  nrIncercari++;
+  if(nrIncercari > maxIncercari){
+    gameContinues = false;
+  }
+  document.getElementById("incercari").style.display = "block";
+  document.getElementById("incercari").innerHTML = 'Nr incercari: ' + nrIncercari + ' din ' + maxIncercari;
+}
+
+function theGame(numberInput) {
+  let rezultatul = theNumber - numberInput;
+  if(gameWon == false){
+    document.getElementById("result").style.display = "block";
+  }
+  switch (true) {
+    case rezultatul > 0:
+      document.getElementById("result").innerHTML = numberInput + ' este prea mic.';
+      document.getElementById('Input1').value = '';
+    break;
+    case rezultatul < 0:
+      document.getElementById("result").innerHTML = numberInput + ' este prea mare.';
+      document.getElementById('Input1').value = '';
+    break;
+    case rezultatul == 0:
+      document.getElementById("result").style.display = "none";
+      document.getElementById("image").style.display = "block";
+      document.getElementById("image").classList.add('image');
+      document.getElementById("Input1").disabled = true;
+      gameWon = true;
+    break;
+    default:
+  }
+}
+
 var my_func = function(event) {
     event.preventDefault();
     let numberInput = parseInt(document.getElementById("Input1").value);
-    if(numberInput){
-      let rezultatul = theNumber - numberInput;
-      if(gameWon == false){
-        document.getElementById("result").style.display = "block";
-      }
-      switch (true) {
-        case rezultatul > 0:
-          document.getElementById("result").innerHTML = numberInput + ' este prea mic.';
-          document.getElementById('Input1').value = '';
-        break;
-        case rezultatul < 0:
-          document.getElementById("result").innerHTML = numberInput + ' este prea mare.';
-          document.getElementById('Input1').value = '';
-        break;
-        case rezultatul == 0:
-          // document.getElementById("result").className = "alert alert-success";
-          document.getElementById("result").style.display = "none";
-          document.getElementById("image").style.display = "block";
-          document.getElementById("image").classList.add('image');
-          document.getElementById("result").innerHTML = numberInput + ' este corect!';
-          gameWon = true;
-        break;
-        default:
+    if(numberInput || numberInput == 0){
+      incercari();
+      if(gameContinues) {
+        theGame(numberInput);
+      } else {
+        document.getElementById("Input1").disabled = true;
+        document.getElementById("result").className = "alert alert-danger";
+        document.getElementById("result").innerHTML = "Game Over!";
       }
     } else {
       alert('Nu ai introdus un numar corect');
